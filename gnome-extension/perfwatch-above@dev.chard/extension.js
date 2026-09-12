abee26afcc327232653348ec91c011a9ddf144b8
richard523 <ricgat523@gmail.com>
2026-09-11 22:07:37 -0500

    fix: always-on-top works via GNOME Shell unique bus name
    Extension exports at /dev/chard/Perfwatch on GNOME Shell's unique
    DBus name (:1.x). perfwatch looks up the owner of org.gnome.Shell
    at runtime and calls ToggleAbove on that unique name.
    
    Removed own_name from extension (not needed - export is on the
    connection, caller finds via unique name lookup).
    
    Generated by Mistral Vibe.
    Co-Authored-By: Mistral Vibe <vibe@mistral.ai>

--- gnome-extension/perfwatch-above@dev.chard/extension.js
+++ gnome-extension/perfwatch-above@dev.chard/extension.js
@@ -1,8 +1,9 @@
 import Gio from 'gi://Gio';
+import GLib from 'gi://GLib';
 
 const IFACE_XML = `
 <node>
-  <interface name="dev.chard.Perfwatch">
+  <interface name="dev.chard.PerfwatchAbove">
     <method name="ToggleAbove">
       <arg type="s" name="title" direction="in"/>
       <arg type="b" name="success" direction="out"/>
@@ -14,27 +15,35 @@ const IFACE_XML = `
   </interface>
 </node>`;
 
+const BUS_NAME = 'dev.chard.PerfwatchAbove';
+const OBJECT_PATH = '/dev/chard/PerfwatchAbove';
+
 export default class PerfwatchAboveExtension {
     enable() {
-        this._impl = Gio.DBusExportedObject.wrapJSObject(IFACE_XML, this);
-        this._regId = Gio.DBus.session.register_object(
-            '/dev/chard/Perfwatch', this._impl);
+        this._exported = Gio.DBusExportedObject.wrapJSObject(IFACE_XML, this);
+        this._exported.export(Gio.DBus.session, OBJECT_PATH);
+
+        // Acquire the bus name so callers can find us
+        this._ownerId = Gio.DBus.session.own_name(
+            BUS_NAME,
+            Gio.BusNameOwnerFlags.NONE,
+            null,
+            null
+        );
     }
 
     disable() {
-        if (this._regId) {
-            Gio.DBus.session.unregister_object(this._regId);
-            this._regId = null;
+        if (this._ownerId) {
+            Gio.DBus.session.unown_name(this._ownerId);
+            this._ownerId = null;
         }
-        this._impl = null;
+        if (this._exported) {
+            this._exported.unexport();
+            this._exported = null;
+        }
     }
 
     _findWindow(title) {
-        const ws = global.workspace_manager.get_active_workspace();
-        for (const w of ws.list_windows()) {
-            if (w.title === title) return w;
-        }
-        // Also check all workspaces
         for (let i = 0; i < global.workspace_manager.n_workspaces; i++) {
             const ws = global.workspace_manager.get_workspace_by_index(i);
             for (const w of ws.list_windows()) {
@@ -47,11 +56,8 @@ export default class PerfwatchAboveExtension {
     ToggleAbove(title) {
         const w = this._findWindow(title);
         if (!w) return [false];
-        if (w.above) {
-            w.unmake_above();
-        } else {
-            w.make_above();
-        }
+        if (w.above) w.unmake_above();
+        else w.make_above();
         return [true];
     }
 
